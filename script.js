209bc4ec0451bbdca293a535cde9271974cfbd7f
Rafael Alberto Larios <albertolarios@me.com>
2022-07-13 21:22:32 -0500

    Add functions to yield result from input
    The functions added address the processing of strings as float numbers
    in the hierarchical order of the binary mathmatical operations (first
    multiplication, then division).

--- script.js
+++ script.js
@@ -25,70 +25,70 @@ function operate(operandA, operandB, operation) {
         case "÷":
             return divide(operandA, operandB);
     }
-}
+};
 
 function clear() {
-    screen.textContent = "0";
-}
+    screenSpan.textContent = "0";
+};
 
 function deleteLast() {
-    const content = screen.textContent;
+    const content = screenSpan.textContent;
     if (content.length != 1) {
-        screen.textContent = content.slice(0, content.length - 1);
+        screenSpan.textContent = content.slice(0, content.length - 1);
     }
     else {
-        screen.textContent = "0";
+        screenSpan.textContent = "0";
     }
-}
+};
 
 const mathSymbols = ["+", "-", "÷", "×"];
 
 function isOperator(symbol) {
     return mathSymbols.some(operator => symbol == operator);
-}
+};
 
 function findLastOperator(array) {
     let index = array.reverse().findIndex(symbol => isOperator(symbol));
     const lastIndex = array.length - 1;
     !(index + 1) ? index = lastIndex : null;
     return lastIndex - index;
-}
+};
 
 function allowPoint(array) {
     return Boolean(!array.find(symbol => symbol == "."))
-}
+};
 
 function stripOperator(string) {
     return mathSymbols.find(symbol => symbol == string[0]) ? string.slice(1) : string;
-}
+};
 
 function extractLastNumber(string) {
     const lastOperator = findLastOperator(string.split(""));
     return stripOperator(string.slice(lastOperator));
-}
+};
 
 function inputMath() {
-    const content = screen.textContent;
+    const content = screenSpan.textContent;
     const lastInput = content.slice(content.length - 1);
     const input = this.textContent;
     const lastNumber = extractLastNumber(content).split("");
     if (content == "0") {
         if (input == "." || isOperator(input)) {
-            screen.textContent = "0" + input;
+            screenSpan.textContent = "0" + input;
         }
         else if (!(input == 0)) {
-            screen.textContent = input;
+            screenSpan.textContent = input;
         }
     }
     else if (isOperator(lastInput)) {
         if (input == ".") {
-            screen.textContent += "0."
+            screenSpan.textContent += "0."
         }
         else if (isOperator(input)) {
-            screen.textContent = content.slice(0, content.length - 1) + input;
+            screenSpan.textContent = content.slice(0, content.length - 1) + input;
         }
         else {
-            screen.textContent += input;
+            screenSpan.textContent += input;
         }
     }
     else  {
@@ -96,15 +96,78 @@ function inputMath() {
             null;
         }
         else if (lastNumber == "0" && input  != ".") {
-            screen.textContent = content.slice(0, content.length - 1) + "0";
+            screenSpan.textContent = content.slice(0, content.length - 1) + "0";
         }
         else {
-            screen.textContent += input;
+            screenSpan.textContent += input;
         }
     }
+};
+
+function findOperatorIndeces(charArray) {
+    indeces = charArray.reduce((array, element, elementIndex) => {
+        if (isOperator(element)) {
+            array.push(elementIndex);
+        }
+        return array;
+    }, [])
+    return indeces;
+};
+
+function reduceString(charArray, precedence) {
+    function isUnresolved() {
+        operatorIndeces = findOperatorIndeces(charArray);
+        switch (precedence) {
+            case "first":
+                nextIndexIndex = operatorIndeces.findIndex(index => /[×÷]/.test(charArray[index]));
+                break;
+            case "second":
+                nextIndexIndex = operatorIndeces.findIndex(index => /[\+-]/.test(charArray[index]));
+                break;
+        }
+        return nextIndexIndex != -1 ? true : false;
+    }
+    let operatorIndeces;
+    let nextIndexIndex;
+    while (isUnresolved()) {
+        let previousOperatorIndex;
+        let nextOperatorIndex;
+        if (nextIndexIndex == 0) {
+            previousOperatorIndex = 0 - 1;
+        }
+        else {
+            previousOperatorIndex = operatorIndeces[nextIndexIndex - 1];
+        }
+        if (nextIndexIndex == operatorIndeces.length - 1) {
+            nextOperatorIndex = charArray.length;
+        }
+        else {
+            nextOperatorIndex = operatorIndeces[nextIndexIndex + 1];
+        }
+        const operatorIndex = operatorIndeces[nextIndexIndex];
+        const operandA = parseFloat(charArray.slice(previousOperatorIndex + 1, operatorIndex).join(""));
+        const operandB = parseFloat(charArray.slice(operatorIndex + 1, nextOperatorIndex).join(""));
+        const operator = charArray[operatorIndex];
+        const result = operate(operandA, operandB, operator);
+        const newString = charArray.slice(0, previousOperatorIndex + 1).join("") +
+            result +
+            charArray.slice(nextOperatorIndex, charArray.length).join("");
+        charArray = newString.split("");
+    }
+    return charArray;
 }
 
-const screen = document.querySelector("#screen span");
+function yieldResult() {
+    let charArray = screenSpan.textContent.split("");
+    charArray = reduceString(charArray, "first");
+    charArray = reduceString(charArray, "second");
+    newLine = document.createElement("span");
+    newLine.textContent = charArray.join("");
+    screen.appendChild(newLine);
+};
+
+const screen = document.querySelector("#screen");
+const screenSpan = document.querySelector("#screen span");
 const buttons = document.querySelectorAll(".printable");
 buttons.forEach(button => button.addEventListener("click", inputMath))
 const clearButton = document.querySelector("#clr");
